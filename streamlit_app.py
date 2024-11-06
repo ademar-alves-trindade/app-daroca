e7539e44609f948e5adeba92edd4e94c79abd93f
ademar-alves-trindade <71803270+ademar-alves-trindade@users.noreply.github.com>
2024-11-06 15:28:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Ademar"
 )
